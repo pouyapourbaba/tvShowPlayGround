b339af49832b5942d86f67efc8e271234e16045d
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-06-09 13:13:16 +0300

    reverted

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,22 @@ import SearchShow from "./components/SearchShow";
 import Favorites from "./components/Favorites";
 import MovieDetail from "./components/MovieDetail";
 import Navbar from "./components/Navbar";
-import {
-  Context,
-  AppContextProvider,
-  AppContextInterface,
-  reducer
-} from "./Store";
 
-const initialState: AppContextInterface = {
-  movies: [],
-  favorites: []
-};
-
-function App(): JSX.Element {
-  // const {state, dispatch} = React.useContext(Context);
-  const [state, dispatch] = React.useReducer(reducer, initialState);
-
-  console.log(state);
+function App() {
   return (
-    <AppContextProvider value={{ state, dispatch }}>
-      <div className={styles["App"]}>
-        <header>
-          <Navbar />
-        </header>
-        <main>
-          <Switch>
-            <Route path="/favorites" component={Favorites} />
-            <Route path="/movies/:id" component={MovieDetail} />
-            <Route exact path="/" component={SearchShow} />
-            <Redirect to="/not-found" />
-          </Switch>
-        </main>
-      </div>
-    </AppContextProvider>
+    <div className={styles["App"]}>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Switch>
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/movies/:id" component={MovieDetail} />
+          <Route exact path="/" component={SearchShow} />
+          <Redirect to="/not-found"/>
+        </Switch>
+      </main>
+    </div>
   );
 }
 
